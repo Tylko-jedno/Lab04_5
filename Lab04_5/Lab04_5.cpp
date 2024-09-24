fb3bf3457ced5c33f552627f437f98b303328d3a
Tylko-jedno <diogkek@gmail.com>
2024-09-24 10:25:09 +0300

    first master commit

---
+++ Lab04_5/Lab04_5.cpp
@@ -0,0 +1,42 @@
+﻿#include <iostream>
+#include <iomanip>
+#include <time.h>
+
+using namespace std;
+
+int main()
+{
+	double x, y, R;
+
+	srand((unsigned)time(NULL));
+
+	for (int i = 0; i < 10; i++)
+	{
+		cout << "x = "; cin >> x;
+		cout << "y = "; cin >> y;
+		cout << "R = "; cin >> R;
+
+		if (((y >= 0) && (x <= 0) && (y * y <= R * R - x * x)) ||
+			((y <= 0) && (y >= -R) && (x >= 0) && (x <= R) && (y >= -2 * x) && (y >= -2 * R + 2 * x)))
+			cout << "yes" << endl;
+		else
+			cout << "no" << endl;
+	}
+	cout << endl << fixed;
+
+	for (int i = 0; i < 10; i++) {
+		
+		x = -R + rand() * (R + R) / RAND_MAX;
+		y = -R + rand() * (R + R) / RAND_MAX;
+		
+		if (((y >= 0) && (x <= 0) && (y * y <= R * R - x * x)) ||
+			((y <= 0) && (y >= -R) && (x >= 0) && (x <= R) && (y >= -2 * x) && (y >= -2 * R + 2 * x)))
+			cout << setw(8) << setprecision(4) << x << " "
+			<< setw(8) << setprecision(4) << y << " " << "yes" << endl;
+		else
+			cout << setw(8) << setprecision(4) << x << " "
+			<< setw(8) << setprecision(4) << y << " " << "no" << endl;
+
+	}
+
+}
